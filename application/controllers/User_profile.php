04d5286d18d99353349f599b22f877f7408fccf1
Martin <martin@click-ap.com>
2023-06-27 15:28:47 +0800

    Fixed #4628, 志工修改個人資料

--- application/controllers/User_profile.php
+++ application/controllers/User_profile.php
@@ -2,19 +2,23 @@
 
 class User_profile extends CI_Controller
 {
-
+    protected $userID;
     public function __construct()
     {
         parent::__construct();
         $this->load->helper('form');
         $this->load->model('user_profile_model');
+        // 這邊之後請改抓SESSION
+        session_start();
+        $_SESSION['userID'] = isset($_SESSION['userID']) ? $_SESSION['userID'] : 1;
+        $this->userID = $_SESSION['userID'];
     }
 
     public function index()
     {
         $data['page_name'] = 'index';
         $data['link_save'] = base_url('user_profile/edit');
-        $data['form'] = $this->user_profile_model->getUserById('18');
+        $data['form'] = $this->user_profile_model->getUserById($this->userID);
         $this->load->view('volunteer_manage/header', array('active' => 'user_profile'));
         $this->load->view('user_profile/user_profile', $data);
         $this->load->view('volunteer_manage/footer');
@@ -24,7 +28,7 @@ class User_profile extends CI_Controller
     {
         $data['page_name'] = 'edit';
         $data['link_save'] = base_url('user_profile/edit');
-        $data['form'] = $this->user_profile_model->getUserById('18');
+        $data['form'] = $this->user_profile_model->getUserById($this->userID);
         if ($_post = $this->input->post()) {
             $this->user_profile_model->updateUser(array_merge($data['form'], $_post));
             redirect(base_url("user_profile"));
